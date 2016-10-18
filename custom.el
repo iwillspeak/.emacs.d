5edba87c2252dbf68dee35bae75b69ffc91192d8
Will Speak <will.speak@crispthinking.com>
2016-10-18 16:11:47 +0100

    Custom.el Updates

--- custom.el
+++ custom.el
@@ -12,13 +12,13 @@
  '(custom-safe-themes
    (quote
 	("32e3693cd7610599c59997fee36a68e7dd34f21db312a13ff8c7e738675b6dfc" default)))
+ '(deft-directory "/Users/will/Notes")
  '(omnisharp--curl-executable-path "C:\\Curl\\AMD64\\CURL.EXE")
  '(omnisharp-server-executable-path
    "C:\\Users\\willspeak\\Source\\MyRepos\\omnisharp-server\\OmniSharp\\bin\\Release\\OmniSharp.exe")
- '(deft-directory "/Users/will/Notes")
  '(package-selected-packages
    (quote
-	(moe-theme yaml-mode use-package toml-mode rust-mode powerline omnisharp neotree multiple-cursors markdown-mode magit leerzeichen gruvbox-theme gitignore-mode gitconfig-mode expand-region deft autopair)))
+	(powershell company moe-theme yaml-mode use-package toml-mode rust-mode powerline omnisharp neotree multiple-cursors markdown-mode magit leerzeichen gruvbox-theme gitignore-mode gitconfig-mode expand-region deft autopair)))
  '(when (not (facep (aref ansi-term-color-vector 0)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
