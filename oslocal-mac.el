79b02cd36589ff3197b3d5deae754ea7125ef5f5
Will Speak <lithiumflame@gmail.com>
2018-12-08 22:59:13 +0000

    Copy Environment from Shell on macOS
    Trying to fixup RLS when running from the Dock. Emacs now appears to
    have the corret environment but the RLS executable still isn't found.

--- oslocal-mac.el
+++ oslocal-mac.el
@@ -7,5 +7,16 @@
 
 (setq powerline-image-apple-rgb t)
 
+;; Fixup the env variables. Neede if launched from Dock or Finder
+(mapcar (lambda (env)
+		  ; grab the parts of the environment variable
+		  (let* ((env-line (split-string env "="))
+				 (env-name (car env-line))
+				 (env-value (cadr env-line)))
+			(setenv env-name env-value)))
+		; Run the `env` command and take each line to process
+		(split-string (shell-command-to-string ". ~/.profile; env")
+					  "\n"))
+
 ;; Because this is what I use on windows
 (global-set-key (kbd "M-<up>") 'toggle-frame-fullscreen)
