098ffcf6febed77768c1326f7dda75e7b7ea10d1
Will Speak <lithiumflame@gmail.com>
2018-12-09 09:03:59 +0000

    Use `jq` for `prettify-json` if Avaialble
    Update the `prettify-json` command to prefer using `jq` for
    formatting. This adds a bit of flexibility and removes the hard
    dependency on Python just for `json.tool`.

--- macros/json.el
+++ macros/json.el
@@ -1,11 +1,14 @@
 ;; Prettyfy Json
 (defun prettify-json ()
-  "Quickly format json with python's `json.tool' module"
+  "Quickly format json with `jq` or python's `json.tool' module."
   (interactive)
   (let ((start (if (use-region-p)
 				  (region-beginning)
 				 (buffer-end -1)))
 		(end (if (use-region-p)
 				 (region-end)
-			   (buffer-end 1))))
-	(shell-command-on-region start end "python -mjson.tool" 'insert 'replace)))
+			   (buffer-end 1)))
+		(tool (if (executable-find "jq")
+				  "jq"
+				"python -mjson.tool")))
+	(shell-command-on-region start end tool 'insert 'replace)))
